a76eed7d0ee89b95b410948a02ca3d9a7b02dbac
joykami1983 <meetcancanyou@yahoo.com>
2014-10-17 17:59:36 +0800

    Ok,Waiting.

--- protected/views/site/pages/about.php
+++ protected/views/site/pages/about.php
@@ -2,11 +2,10 @@
 /* @var $this SiteController */
 
 $this->pageTitle=Yii::app()->name . ' - About';
-$this->breadcrumbs=array(
-	'About',
-);
+
 ?>
 <h1>About</h1>
 
-<p>This is a "static" page. You may change the content of this page
-by updating the file <code><?php echo __FILE__; ?></code>.</p>
+<p>
+    Paypal for the rapid completion of the payment and payment after the association process provides a simple safe interface program.
+</p>
