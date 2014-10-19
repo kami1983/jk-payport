7e0cf9648a50de8b049ad169d9eac97011aae8bc
linhai <linhai@cancanyou.com>
2014-10-19 22:05:36 +0800

    linhai commit developer paypal somethings.

--- protected/controllers/PaypalController.php
+++ protected/controllers/PaypalController.php
@@ -13,7 +13,7 @@ class PaypalController extends Controller
      */
     public function __construct($id,$module=null)
     {
-        $composerAutoload = dirname(__DIR__) . '/../../paypal-sdk/autoload.php';
+        $composerAutoload = dirname(__DIR__) . '/../../paypal-sdk/vendor/autoload.php';
         echo $composerAutoload ;
         echo 'RUN 2 ';
         if (!file_exists($composerAutoload)) {
