47ecd78a02056790bac86a1cbd5f6faa80c30f18
linhai <linhai@cancanyou.com>
2014-10-20 08:23:02 +0800

    linhai commit developer paypal somethings.

--- protected/controllers/PaypalController.php
+++ protected/controllers/PaypalController.php
@@ -24,12 +24,12 @@ class PaypalController extends Controller
             Yii::app()->end();
         }
         
-        require $composerAutoload;
-        require dirname(__DIR__) . '/../../paypal-sdk/sample/common.php';
-
-//        use PayPal\Rest\ApiContext;
-//        use PayPal\Auth\OAuthTokenCredential;
-        error_reporting(E_ALL);
+//        require $composerAutoload;
+//        require dirname(__DIR__) . '/../../paypal-sdk/sample/common.php';
+//
+////        use PayPal\Rest\ApiContext;
+////        use PayPal\Auth\OAuthTokenCredential;
+//        error_reporting(E_ALL);
         
         // Replace these values by entering your own ClientId and Secret by visiting https://developer.paypal.com/webapps/developer/applications/myapps
         $clientId = 'AYSq3RDGsmBLJE-otTkBtM-jBRd1TCQwFf9RGfwddNXWz0uFU9ztymylOhRS';
@@ -41,19 +41,8 @@ class PaypalController extends Controller
         
     }
     
-    /**
-     * paypal 付款类的构造方法，一些参数初始化应该隔离在这里
-     * @param string $id id of this controller
-     * @param CWebModule $module the module that this controller belongs to.
-     */
-    public function __construct($id,$module=null)
-    {
-        $this->apiContext();
-        //调用父类否则VIEW 无法解析
-        parent::__construct($id, $module);
-    }
     
-   /**
+    /**
     * Helper method for getting an APIContext for all calls
     *
     * @return PayPal\Rest\ApiContext
@@ -103,7 +92,19 @@ class PaypalController extends Controller
        return $apiContext;
    }
 
-
+    
+    /**
+     * paypal 付款类的构造方法，一些参数初始化应该隔离在这里
+     * @param string $id id of this controller
+     * @param CWebModule $module the module that this controller belongs to.
+     */
+    public function __construct($id,$module=null)
+    {
+        $this->apiContext();
+        //调用父类否则VIEW 无法解析
+        parent::__construct($id, $module);
+    }
+    
     /**
      * Declares class-based actions.
      */
@@ -138,7 +139,116 @@ class PaypalController extends Controller
      * 创建付款页面
      */
     public function actionPayment(){
+        //Yii::app()->request->getParam(''); 
+        
+        // ### Payer
+        // A resource representing a Payer that funds a payment
+        // For paypal account payments, set payment method
+        // to 'paypal'.
+        $payer = new Payer();
+        $payer->setPaymentMethod("paypal");
 
+        // ### Itemized information
+        // (Optional) Lets you specify item wise
+        // information
+        $item1 = new Item();
+        $item1->setName('Ground Coffee 40 oz')
+                ->setCurrency('USD')
+                ->setQuantity(1)
+                ->setPrice('7.50');
+        $item2 = new Item();
+        $item2->setName('Granola bars')
+                ->setCurrency('USD')
+                ->setQuantity(5)
+                ->setPrice('2.00');
+
+        $itemList = new ItemList();
+        $itemList->setItems(array($item1, $item2));
+
+        // ### Additional payment details
+        // Use this optional field to set additional
+        // payment information such as tax, shipping
+        // charges etc.
+        $details = new Details();
+        $details->setShipping('1.20')
+                ->setTax('1.30')
+                ->setSubtotal('17.50');
+
+        // ### Amount
+        // Lets you specify a payment amount.
+        // You can also specify additional details
+        // such as shipping, tax.
+        $amount = new Amount();
+        $amount->setCurrency("USD")
+                ->setTotal("20.00")
+                ->setDetails($details);
+
+        // ### Transaction
+        // A transaction defines the contract of a
+        // payment - what is the payment for and who
+        // is fulfilling it. 
+        $transaction = new Transaction();
+        $transaction->setAmount($amount)
+                ->setItemList($itemList)
+                ->setDescription("Payment description");
+
+        // ### Redirect urls
+        // Set the urls that the buyer must be redirected to after 
+        // payment approval/ cancellation.
+        $baseUrl = getBaseUrl();
+        $redirectUrls = new RedirectUrls();
+        $redirectUrls->setReturnUrl("$baseUrl/ExecutePayment.php?success=true")
+                ->setCancelUrl("$baseUrl/ExecutePayment.php?success=false");
+
+        // ### Payment
+        // A Payment Resource; create one using
+        // the above types and intent set to 'sale'
+        $payment = new Payment();
+        $payment->setIntent("sale")
+                ->setPayer($payer)
+                ->setRedirectUrls($redirectUrls)
+                ->setTransactions(array($transaction));
+
+        // ### Create Payment
+        // Create a payment by calling the 'create' method
+        // passing it a valid apiContext.
+        // (See bootstrap.php for more on `ApiContext`)
+        // The return object contains the state and the
+        // url to which the buyer must be redirected to
+        // for payment approval
+        try {
+                $payment->create($apiContext);
+        } catch (PayPal\Exception\PPConnectionException $ex) {
+                echo "Exception: " . $ex->getMessage() . PHP_EOL;
+                var_dump($ex->getData());	
+                exit(1);
+        }
+
+        // ### Get redirect url
+        // The API response provides the url that you must redirect
+        // the buyer to. Retrieve the url from the $payment->getLinks()
+        // method
+        foreach($payment->getLinks() as $link) {
+                if($link->getRel() == 'approval_url') {
+                        $redirectUrl = $link->getHref();
+                        break;
+                }
+        }
+
+        // ### Redirect buyer to PayPal website
+        // Save the payment id so that you can 'complete' the payment
+        // once the buyer approves the payment and is redirected
+        // back to your website.
+        //
+        // It is not a great idea to store the payment id
+        // in the session. In a real world app, you may want to 
+        // store the payment id in a database.
+        $_SESSION['paymentId'] = $payment->getId();
+        if(isset($redirectUrl)) {
+                header("Location: $redirectUrl");
+                exit;
+        }
+        
     }
 
 }
