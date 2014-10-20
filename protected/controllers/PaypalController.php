8cac7663858a5b300f0ffe0f4868485ead905cd8
joykami1983 <meetcancanyou@yahoo.com>
2014-10-20 16:04:51 +0800

    This is linhai develop paypal payment.

--- protected/controllers/PaypalController.php
+++ protected/controllers/PaypalController.php
@@ -22,8 +22,8 @@ use PayPal\Api\FundingInstrument;
 
 //use PayPal\Api\Amount;
 //use PayPal\Api\Details;
-//use PayPal\Api\Item;
-//use PayPal\Api\ItemList;
+use PayPal\Api\Item;
+use PayPal\Api\ItemList;
 //use PayPal\Api\CreditCard;
 //use PayPal\Api\Payer;
 //use PayPal\Api\Payment;
