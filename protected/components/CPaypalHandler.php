b0157143f2747ede5e7fdb82ae79988d5170a4ae
joykami1983 <meetcancanyou@yahoo.com>
2014-10-22 16:33:05 +0800

    This is linhai develop paypal payment. make interface. 1022

--- protected/components/CPaypalHandler.php
+++ protected/components/CPaypalHandler.php
@@ -314,9 +314,6 @@ class CPaypalHandler extends CBase {
         // The API response provides the url that you must redirect
         // the buyer to. Retrieve the url from the $payment->getLinks()
         // method
-        echo '<br/>';
-        echo $payment->getId();
-        echo '<br/>';
         foreach($payment->getLinks() as $link) {
                 if($link->getRel() == 'approval_url') {
                         $redirectUrl = $link->getHref();
