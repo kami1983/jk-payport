81a198edc4535d68030f31cdf4501f6d1c26f6cd
joykami1983 <meetcancanyou@yahoo.com>
2014-10-27 14:20:32 +0800

    This is linhai develop paypal payment. test ipn. 141027

--- protected/components/CUser.php
+++ protected/components/CUser.php
@@ -15,6 +15,7 @@ class CUser {
     public static function CheckValid($uid,$masksign){
         
         $user_def=self::GetAccountDefined($uid);
+        if(null === $user_def)return false;
         
         //判断是否相等，相等则验证成功
         if($masksign == md5($uid.$user_def['token'])){
@@ -31,6 +32,8 @@ class CUser {
         //定义几个用户
         $user_def=array();
         $user_def['1']=array('token'=>'token_141022_1031');
+        
+        if(!isset($user_def[$uid]))return null;
         
         return $user_def[$uid];
     }
