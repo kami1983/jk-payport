126d9477710364f97ca467a721053d04a5bc314d
linhai <linhai@cancanyou.com>
2014-10-27 22:50:12 +0800

    linhai commit developer paypal somethings. 2014-10-27 home on Monday

--- protected/components/CPaypalHandler.php
+++ protected/components/CPaypalHandler.php
@@ -143,10 +143,8 @@ class CPaypalHandler extends CBase {
        $config_arr=array();
        
        if(YII_DEBUG){
-           echo 'RUN 0 ';
            $config_arr['mode']='sandbox';
        }else{
-           echo 'RUN 1 ';
            $config_arr['mode']='live';
        }
        $config_arr['http.ConnectionTimeOut']=30;
