024182dd60facb8366d1c82e3ecc7f36e2474d23
joykami1983 <meetcancanyou@yahoo.com>
2014-10-29 11:48:54 +0800

    On company 14 1029

--- protected/controllers/PaypalController.php
+++ protected/controllers/PaypalController.php
@@ -152,6 +152,7 @@ class PaypalController extends Controller {
        
         $api_creater=new CPaypalApiCreater($client_id,$client_secret,PUB_PAYPAL_SDK_DIR);
         echo 'RUN 1 ';
+        exit;
         $paypal_handler=new CPaypalHandler($api_creater->getApiContext());
         $paypal_handler->setReturnUrl($return_url);
         $paypal_handler->setCancelUrl($cancel_url);
