b4058182456ff439d66a525dc41d91f3350fc9ea
linhai <linhai@cancanyou.com>
2014-10-29 23:23:48 +0800

    Test .gitignore file

--- protected/config/userlist.conf.php
+++ protected/config/userlist.conf.php
@@ -1,6 +1,6 @@
 <?php
 
-$user_def=array();
+$user_def=array(); 
 $user_def['1']=array('token'=>'token_141022_1031',
                              'client_id'=>'AfSbYRAe0Li9JullQ41NFRZrSlOyDrs_TnOzwmXio7uk8-0TOS86vYWXRsF-',);
 
