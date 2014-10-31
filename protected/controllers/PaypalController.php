8e71a2756da5c36b83353e9a175d4220c15fff04
joykami1983 <meetcancanyou@yahoo.com>
2014-10-31 17:53:34 +0800

    nothing.

--- protected/controllers/PaypalController.php
+++ protected/controllers/PaypalController.php
@@ -85,7 +85,7 @@ class PaypalController extends Controller {
 //            throw new Exception('无法识别调用用户','141022_1027');
 //        }
         
-        echo 'RUN action IPN <br/>';
+        echo 'Pay do.';
         
         ###########
         Yii::trace(date('Y-m-d H-i-s')."\n".print_r($_GET,true), 'jkdebug.PaypalController.actionIpn');
