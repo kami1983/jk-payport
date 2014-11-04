4a5bea06599fe897d5ed637c6b0c84e013e50649
linhai <linhai@cancanyou.com>
2014-11-04 22:19:41 +0800

    Developer user login 141104 on home.

--- protected/views/paypal/paymentlist.php
+++ protected/views/paypal/paymentlist.php
@@ -10,14 +10,16 @@
     <tr>
         <td>Id</td>
         <td>IP</td>
-        <td>Payment Info</td>
-        <td>Payment Get</td>
-        <td>Payment Post</td>
+        <td style="width:100px;">Do Response</td>
+        <td style="width:100px;">Payment Info</td>
+        <td style="width:100px;">Payment Get</td>
+        <td style="width:100px;">Payment Post</td>
         
     </tr>
     <?php foreach($payportmentobj_arr as $payportmentobj): ?>
     <tr>
         <td><?php echo $payportmentobj->id; ?></td>
+        <td><?php echo $payportmentobj->do_response; ?></td>
         <td><?php echo $payportmentobj->ipaddress; ?></td>
         <td><?php echo $payportmentobj->payment_json; ?></td>
         <td><?php echo $payportmentobj->get_json; ?></td>
