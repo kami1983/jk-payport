de3f4630dcca960055fa0d3e95fcffb77b5e11c5
linhai <linhai@cancanyou.com>
2014-11-05 08:03:07 +0800

    Developer user login 141105 on home.

--- protected/views/paypal/paymentlist.php
+++ protected/views/paypal/paymentlist.php
@@ -7,7 +7,7 @@
 //echo '</pre>'; http://develop.jk-payport.git.cancanyou.com/index.php?r=paypal/paymentlist
 ?>
 <form action="<?php echo $this->createUrl('paypal/showjson');?>" method="POST" target="_black">
-    <input name="json_str" type="text" value="" />
+    <input id="id_json_str" name="json_str" type="text" value="" />
 <table>
     <tr>
         <td>Id</td>
@@ -24,22 +24,22 @@
         <td><?php echo $payportmentobj->ipaddress; ?></td>
         <td>
             <?php if('' != $payportmentobj->do_response): ?>
-            <input type='submit' value="do_response" onclick="$('[name=json_str]').val('<?php echo base64_encode($payportmentobj->do_response); ?>');" />
+            <input type='submit' value="do_response" onclick="$('#id_json_str').val('<?php echo base64_encode($payportmentobj->do_response); ?>');" />
             <?php endif; ?>
         </td>
         <td>
             <?php if('' != $payportmentobj->payment_json): ?>
-            <input type='submit' value="payment_json" onclick="$('[name=json_str]').val('<?php echo base64_encode($payportmentobj->payment_json); ?>');" />
+            <input type='submit' value="payment_json" onclick="$('#id_json_str').val('<?php echo base64_encode($payportmentobj->payment_json); ?>');" />
             <?php endif; ?>
         </td>
         <td>
             <?php if('' != $payportmentobj->get_json): ?>
-            <input type='submit' value="get_json" onclick="$('[name=json_str]').val('<?php echo base64_encode($payportmentobj->get_json); ?>');" />
+            <input type='submit' value="get_json" onclick="$('#id_json_str').val('<?php echo base64_encode($payportmentobj->get_json); ?>');" />
             <?php endif; ?>
         </td>
         <td>
             <?php if('' != $payportmentobj->post_json): ?>
-            <input type='submit' value="post_json" onclick="$('[name=json_str]').val('<?php echo base64_encode($payportmentobj->post_json); ?>');" />
+            <input type='submit' value="post_json" onclick="$('#id_json_str').val('<?php echo base64_encode($payportmentobj->post_json); ?>');" />
             <?php endif; ?>
         </td>
     </tr>
