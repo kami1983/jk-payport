ff633bde53c155853bb81f0b04e728105cc59037
linhai <linhai@cancanyou.com>
2014-11-19 21:09:30 +0800

    Add feature setting config. 2014-11-19

--- protected/views/site/setting.php
+++ protected/views/site/setting.php
@@ -3,29 +3,38 @@
 /* @var $adminlist_conf_arr array */
 /* @var $userlist_conf_arr array */
 
-print_r($userlist_conf_arr);
 ?>
 
 <h1>Set admin list.</h1>
+
 <hr/>
 
 <form action='<?php $this->createUrl('setting', array()); ?>'  >
 <?php foreach($adminlist_conf_arr as $name=>$pwd): ?>
 <p>
-用户名：<input name='adminlist_conf_arr_name[]' type='text' value='<?php echo $name; ?>' />
-密码：<input name='adminlist_conf_arr_pwd[]' type='text' value='<?php echo $pwd; ?>' />
+修改用户名：<input name='adminlist_conf_arr_name[]' type='text' value='<?php echo $name; ?>' />
+修改密码：<input name='adminlist_conf_arr_pwd[]' type='text' value='<?php echo $pwd; ?>' />
 </p>
 <?php endforeach; ?>
+<p>
+新增用户名：<input name='adminlist_conf_arr_name[]' type='text' value='' />
+新增密码：<input name='adminlist_conf_arr_pwd[]' type='text' value='' />
+</p>
 <input type='submit' value='SUBMIT' />
 </form>
 
+<hr/>
 
 <form action='<?php $this->createUrl('setting', array()); ?>'  >
 <?php foreach($userlist_conf_arr as $conf_arr): ?>
 <p>
-TOKEN：<input name='adminlist_conf_arr_token[]' type='text' value='<?php echo $conf_arr['token']; ?>' />
-CLIENT ID：<input name='adminlist_conf_arr_client_id[]' type='text' value='<?php echo $conf_arr['client_id']; ?>' />
+修改 TOKEN：<input name='adminlist_conf_arr_token[]' type='text' value='<?php echo $conf_arr['token']; ?>' />
+修改 CLIENT ID：<input name='adminlist_conf_arr_client_id[]' type='text' value='<?php echo $conf_arr['client_id']; ?>' />
 </p>
 <?php endforeach; ?>
+<p>
+新增 TOKEN：<input name='adminlist_conf_arr_token[]' type='text' value='' />
+新增 CLIENT ID：<input name='adminlist_conf_arr_client_id[]' type='text' value='' />
+</p>
 <input type='submit' value='SUBMIT' />
 </form>
