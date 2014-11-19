7048362d60c280e1dcec25655f7b6a7c79b53aaf
joykami1983 <meetcancanyou@yahoo.com>
2014-11-19 17:28:57 +0800

    Add Setting page. 14-11-18

--- protected/views/site/setting.php
+++ protected/views/site/setting.php
@@ -8,10 +8,14 @@
 
 <h1>Set admin list.</h1>
 <hr/>
+
+<form action='<?php $this->createUrl('setting', array()); ?>'  >
 <?php foreach($adminlist_conf_arr as $name=>$pwd): ?>
 <p>
-用户名：<?php echo $name; ?> 
-密码：<?php echo $pwd; ?> 
+用户名：<input name='adminlist_conf_arr_name[]' type='text' value='<?php echo $name; ?>' />
+密码：<input name='adminlist_conf_arr_pwd[]' type='text' value='<?php echo $pwd; ?>' />
 </p>
 <?php endforeach; ?>
+<input type='submit' value='SUBMIT' />
+</form>
 
