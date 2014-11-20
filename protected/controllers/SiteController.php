be01b77b50321dc120438271c825aac7bfb8b833
joykami1983 <meetcancanyou@yahoo.com>
2014-11-20 16:22:29 +0800

    Add Setting page. 14-11-20

--- protected/controllers/SiteController.php
+++ protected/controllers/SiteController.php
@@ -156,11 +156,11 @@ class SiteController extends Controller
                     Yii::app()->mailer->SMTPAuth = true;
                 }
 
-                Yii::app()->mailer->From = $emailsmtp_conf_arr['smtp_host'];
+                Yii::app()->mailer->From = $emailsmtp_conf_arr['smtp_user'];
                 Yii::app()->mailer->FromName = "Payport email service test.";
                 Yii::app()->mailer->AddAddress($test_emailaddress,"Your name.");    
 //                Yii::app()->mailer->AddAddress("kami@cancanyou.com", "Hello Kami");
-                Yii::app()->mailer->AddReplyTo($emailsmtp_conf_arr['smtp_host'], "Payport Service");
+                Yii::app()->mailer->AddReplyTo($emailsmtp_conf_arr['smtp_user'], "Payport Service");
 
         //        Yii::app()->mailer->WordWrap = 50;                                 // set word wrap to 50 characters
         //        Yii::app()->mailer->AddAttachment("/var/tmp/file.tar.gz");         // add attachments
