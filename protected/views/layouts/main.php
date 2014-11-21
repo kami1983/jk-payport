9c99a2060b2520f4cf233b3c6d166d7e0086f60a
joykami1983 <meetcancanyou@yahoo.com>
2014-11-21 12:15:06 +0800

    publis version 1.0.9

--- protected/views/layouts/main.php
+++ protected/views/layouts/main.php
@@ -23,7 +23,7 @@
 <div class="container" id="page">
 
 	<div id="header">
-		<div id="logo"><?php echo CHtml::encode(Yii::app()->name); ?> - 1.0.8</div>
+		<div id="logo"><?php echo CHtml::encode(Yii::app()->name); ?> - 1.0.9</div>
 	</div><!-- header -->
 
 	<div id="mainmenu">
